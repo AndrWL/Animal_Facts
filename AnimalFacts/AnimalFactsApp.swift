013ac0477efbc27d147c3a8ec3ee7cd9ce647e31
AndrWL <andrewlebedev13@gmail.com>
2025-09-10 13:17:49 +0300

    Initial Commit

---
+++ AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Created by ANDRII LEBEDIEV on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
